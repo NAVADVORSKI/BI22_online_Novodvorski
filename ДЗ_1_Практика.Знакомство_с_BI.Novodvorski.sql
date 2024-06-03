2853a615336390478f8e2e948a4007b9ea151ead
NAVADVORSKI <vladnovodvorski@gmail.com>
2024-06-03 16:02:50 +0300

    Add files via upload

---
+++ ДЗ_1_Практика.Знакомство_с_BI.Novodvorski.sql
@@ -0,0 +1,9 @@
+/* a) SELECT * FROM Sales.SalesTerritory */
+
+/* SELECT TerritoryID, Name FROM Sales.SalesTerritory */
+
+/* SELECT * FROM Person.Person
+WHERE LastName = 'Norman' */
+
+SELECT * FROM Person.Person
+WHERE EmailPromotion <> 2
